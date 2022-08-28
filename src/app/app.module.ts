1f1b8e55c9c1c3cff931466c468889d3383c1755
Sharmane <sjmburlaza@gmail.com>
2022-08-28 22:29:42 +0800

    generate new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UserProfileComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
